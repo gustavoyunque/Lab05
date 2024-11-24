3530fd3b831b75c0930236318469df2b1441f566
gyunque <gyunque@unsa.edu.pe>
2024-11-24 02:39:03 -0500

    Aqui agregamos los modelos basicos para nuestra app que consideramos necesarias

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,20 @@
+
 from django.db import models
 
-# Create your models here.
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    autor = models.CharField(max_length=100)
+    fecha_publicacion = models.DateField()
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.titulo
+
+class Resena(models.Model):
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    nombre_usuario = models.CharField(max_length=100)
+    texto = models.TextField()
+    calificacion = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.nombre_usuario} - {self.libro.titulo}"
