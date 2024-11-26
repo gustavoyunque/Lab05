d4bd898695ddbcbab479a006bc5945e9f89a1608
JafetLlave <jllaveag@unsa.edu.pe>
2024-11-26 11:52:55 -0500

    Vistas creadas para lista_libros y detalles_libro

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Libro
 
-# Create your views here.
+def lista_libros(request):
+    libros = Libro.objects.all()
+    return render(request, 'lista_libros.html', {'libros': libros})
+
+def detalles_libro(request, pk):
+    libro = get_object_or_404(Libro, pk=pk)
+    return render(request, 'detalles_libro.html', {'libro': libro})
+
