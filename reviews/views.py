cab5e8b16e4e7c839f6306c9e6cd49c3370ec7a9
JafetLlave <jllaveag@unsa.edu.pe>
2024-11-26 13:39:57 -0500

    Se agregó el libro EL GATO NEGRO para probar el funcionamiento del servidor y de la base de datos (funciona bien)

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Libro
+from .models import Resena
 
 def lista_libros(request):
     libros = Libro.objects.all()  # Obtiene todos los libros
@@ -8,3 +9,7 @@ def lista_libros(request):
 def detalles_libro(request, pk):
     libro = get_object_or_404(Libro, pk=pk)  # Busca el libro por su ID (pk)
     return render(request, 'reviews/detalles_libro.html', {'libro': libro})
+
+def lista_resenas(request):
+    resenas = Resena.objects.all()  # Obtiene todas las reseñas
+    return render(request, 'reviews/lista_resenas.html', {'resenas': resenas})
