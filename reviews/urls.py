84014dff1ff156a48f2da04c37b331907f7a42ac
gyunque <gyunque@unsa.edu.pe>
2024-12-01 16:57:36 -0500

    Se han implementado vistas y las URL para usuario y libro

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path('libros/', views.lista_libros, name='lista_libros'),
     path('libros/<int:pk>/', views.detalles_libro, name='detalles_libro'),
-    path('resenas/', views.lista_resenas, name='lista_resenas'),  # Nueva URL para las reseñas
+    path('resenas/', views.lista_resenas, name='lista_resenas'),
+    
+    path('libro/crear/', views.crear_libro, name='crear_libro'),
+    path('usuario/crear/', views.crear_usuario, name='crear_usuario'),
+    path('usuarios/', views.lista_usuarios, name='lista_usuarios'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,15 +1,40 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Libro
-from .models import Resena
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Libro, Resena, Usuario  
+from .forms import FormularioUsuario, FormularioLibro
+
 
 def lista_libros(request):
-    libros = Libro.objects.all()  # Obtiene todos los libros
+    libros = Libro.objects.all()
     return render(request, 'reviews/lista_libros.html', {'libros': libros})
 
 def detalles_libro(request, pk):
-    libro = get_object_or_404(Libro, pk=pk)  # Busca el libro por su ID (pk)
+    libro = get_object_or_404(Libro, pk=pk)
     return render(request, 'reviews/detalles_libro.html', {'libro': libro})
 
 def lista_resenas(request):
-    resenas = Resena.objects.all()  # Obtiene todas las reseñas
+    resenas = Resena.objects.all()
     return render(request, 'reviews/lista_resenas.html', {'resenas': resenas})
+
+def crear_libro(request):
+    if request.method == 'POST':
+        formulario = FormularioLibro(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('lista_libros')
+    else:
+        formulario = FormularioLibro()
+    return render(request, 'reviews/formulario_libro.html', {'formulario': formulario})
+
+def crear_usuario(request):
+    if request.method == 'POST':
+        formulario = FormularioUsuario(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('lista_usuarios')
+    else:
+        formulario = FormularioUsuario()
+    return render(request, 'reviews/formulario_usuario.html', {'formulario': formulario})
+
+def lista_usuarios(request):
+    usuarios = Usuario.objects.all()
+    return render(request, 'reviews/lista_usuarios.html', {'usuarios': usuarios})
